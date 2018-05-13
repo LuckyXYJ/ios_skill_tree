6d00a738866ee44099270d52168b91356fa92203
邢雅杰 <470446087@qq.com>
2018-05-13 21:13:12 +0800

    动态方法解析

---
+++ OC/OC_Runtime/OC_Runtime/ResolveMethod.h
@@ -0,0 +1,20 @@
+//
+//  ResolveMethod.h
+//  OC_Runtime
+//
+//  Created by xyj on 2018/5/13.
+//  Copyright © 2018 xyj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ResolveMethod : NSObject
+
+- (void)test;
++ (void)test;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- OC/OC_Runtime/OC_Runtime/ViewController.m
+++ OC/OC_Runtime/OC_Runtime/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 #import <objc/runtime.h>
 #import "XZPerson.h"
+#import "ResolveMethod.h"
 
 @interface ViewController ()
 
@@ -31,14 +32,31 @@
     btn.backgroundColor = [UIColor blueColor];
     [self.view addSubview:btn];
     
+    UIButton *btn1 = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn1.frame = CGRectMake(200, 100, 100, 40);
+    [btn1 addTarget:self action:@selector(clickbutton1) forControlEvents:UIControlEventTouchUpInside];
+    btn1.backgroundColor = [UIColor redColor];
+    [self.view addSubview:btn1];
+    
 }
 
+// 结构题位域，联合体
 - (void)clickbutton {
     
     XZPerson *persion = [[XZPerson alloc]init];
     
     NSLog(@"%d", persion.isHandsome);
     NSLog(@"%d", persion.isRich);
+    
+}
+
+// 动态方法解析
+- (void)clickbutton1 {
+    
+    ResolveMethod *persion = [[ResolveMethod alloc]init];
+    
+    [persion test];
+    [ResolveMethod test];
 }
 
 - (void)didReceiveMemoryWarning {
