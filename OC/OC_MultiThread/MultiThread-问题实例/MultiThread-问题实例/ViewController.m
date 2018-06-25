7f8ad8cbd81e184a6e20c193a7465ae0d93e7513
邢雅杰 <470446087@qq.com>
2018-06-25 21:52:12 +0800

    多线程实例

--- OC/OC_MultiThread/MultiThread-问题实例/MultiThread-问题实例/ViewController.m
+++ OC/OC_MultiThread/MultiThread-问题实例/MultiThread-问题实例/ViewController.m
@@ -8,7 +8,14 @@
 
 #import "ViewController.h"
 
+#import "GCDDemo.h"
+
+#import "ThreadAndRunloop.h"
+
 @interface ViewController ()
+
+@property(nonatomic, strong)GCDDemo *gcdDemo;
+@property(nonatomic, strong)ThreadAndRunloop *runloopDemo;
 
 @end
 
@@ -16,43 +23,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+//     Do any additional setup after loading the view, typically from a nib.
+    _gcdDemo = [[GCDDemo alloc]init];
+    _runloopDemo = [[ThreadAndRunloop alloc]init];
 }
 
-- (void)test2
-{
-    dispatch_queue_t queue = dispatch_get_global_queue(0, 0);
-    
-    dispatch_async(queue, ^{
-        NSLog(@"1");
-        // 这句代码的本质是往Runloop中添加定时器
-        [self performSelector:@selector(test) withObject:nil];
-        NSLog(@"3");
-        
-        //        [[NSRunLoop currentRunLoop] addPort:[[NSPort alloc] init] forMode:NSDefaultRunLoopMode];
-//        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
-        
-        
-    });
+- (IBAction)test1:(id)sender {
+    [_gcdDemo gcdDemo01];
 }
 
-- (void)test
-{
-    NSLog(@"2");
+- (IBAction)test2:(id)sender {
+    [_gcdDemo gcdDemo02];
+}
+
+- (IBAction)test3:(id)sender {
+    [_gcdDemo gcdDemo03];
+}
+
+- (IBAction)test4:(id)sender {
+    [_gcdDemo gcdDemo04];
+}
+
+- (IBAction)test5:(id)sender {
+    [_gcdDemo gcdDemo05];
+}
+
+- (IBAction)test6:(id)sender {
+    [_runloopDemo threadAndRunloopDemo];
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    NSThread *thread = [[NSThread alloc] initWithBlock:^{
-        NSLog(@"1");
-        
-        [[NSRunLoop currentRunLoop] addPort:[[NSPort alloc] init] forMode:NSDefaultRunLoopMode];
-        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
-        
-        NSLog(@"end");
-    }];
-    [thread start];
     
-    [self performSelector:@selector(test2) onThread:thread withObject:nil waitUntilDone:YES];
 }
 
 
