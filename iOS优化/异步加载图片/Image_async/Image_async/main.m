be6c00fe5bc7ac0f304f80efb5673f766d763c28
邢雅杰 <470446087@qq.com>
2018-09-09 19:33:10 +0800

    runloop卡顿检测

---
+++ iOS优化/异步加载图片/Image_async/Image_async/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Image_async
+//
+//  Created by xyj on 2018/9/6.
+//  Copyright © 2018年 xyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
