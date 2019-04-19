d28a83328311ad854dd9d0907787d4cbbd5e6de3
邢雅杰 <470446087@qq.com>
2019-04-19 20:31:12 +0800

    OpenGLES 加载图片

---
+++ iOS视觉/OpenGLES实例/OpenGLES_02_加载图片/OpenGLESI_加载图片/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenGLESI_加载图片
+//
+//  Created by xyj on 2019/4/18.
+//  Copyright © 2019年 xyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
