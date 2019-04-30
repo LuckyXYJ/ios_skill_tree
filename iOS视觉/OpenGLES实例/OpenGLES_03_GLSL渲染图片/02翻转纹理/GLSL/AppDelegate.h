9b8aa82e4c507f9d8dad90be076c999971d878ba
邢雅杰 <470446087@qq.com>
2019-04-30 21:31:12 +0800

    GLSL翻转纹理

---
+++ iOS视觉/OpenGLES实例/OpenGLES_03_GLSL渲染图片/02翻转纹理/GLSL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GLSL
+//
+//  Created by xyj on 2019/4/26.
+//  Copyright © 2017年 xyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
