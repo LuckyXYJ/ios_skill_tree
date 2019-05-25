061ad21e8c535424a09df6e405d72578cd91a2b2
邢雅杰 <470446087@qq.com>
2019-05-25 20:45:12 +0800

    GLSLES 粒子系统

---
+++ iOS视觉/OpenGLES实例/OpenGLES_09_粒子系统/OpenGLES_粒子系统/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenGLES_粒子系统
+//
+//  Created by xyj on 2019/5/25.
+//  Copyright © 2019年 xyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
