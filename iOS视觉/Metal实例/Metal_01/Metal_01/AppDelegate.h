59755e0ec0bfa8dd93f16939b2085151f5126b6e
邢雅杰 <470446087@qq.com>
2019-06-12 20:45:12 +0800

    Metal demo

---
+++ iOS视觉/Metal实例/Metal_01/Metal_01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Metal_01
+//
+//  Created by xyj on 2019/6/12.
+//  Copyright © 2019年 xyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
