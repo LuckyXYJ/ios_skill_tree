244a98dbd3abf838f91ea5f4d76e9ebef06862e8
邢雅杰 <470446087@qq.com>
2019-06-26 20:45:12 +0800

    Metal 相机

---
+++ iOS视觉/Metal实例/Metal_07_RenderCamera/Metal_RenderCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Metal_RenderCamera
+//
+//  Created by xyj on 2019/6/25.
+//  Copyright © 2019年 xyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
