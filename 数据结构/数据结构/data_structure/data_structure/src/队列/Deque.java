3bcc0e6d4466ae9fda2973daa0f7aa88fb69ab7f
邢雅杰 <470446087@qq.com>
2019-10-03 20:41:12 +0800

    双端队列

---
+++ 数据结构/数据结构/data_structure/data_structure/src/队列/Deque.java
@@ -0,0 +1,47 @@
+package 队列;
+
+import com.xyj.list.List;
+import 链表.LinkedList;
+
+/*
+    双端队列
+ */
+public class Deque<E> {
+    private List<E> list = new LinkedList<>();
+
+    public int size() {
+        return list.size();
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public void clear() {
+        list.clear();
+    }
+
+    public void enQueueRear(E element) {
+        list.add(element);
+    }
+
+    public E deQueueFront() {
+        return list.remove(0);
+    }
+
+    public void enQueueFront(E element) {
+        list.add(0, element);
+    }
+
+    public E deQueueRear() {
+        return list.remove(list.size() - 1);
+    }
+
+    public E front() {
+        return list.get(0);
+    }
+
+    public E rear() {
+        return list.get(list.size() - 1);
+    }
+}
