f46c196e5ba1fa1f664142a2dd8e1286ae309a6e
邢雅杰 <470446087@qq.com>
2019-10-24 20:41:12 +0800

    平衡二叉搜索树

--- 数据结构/数据结构/data_structure/data_structure/src/二叉树/AVLTree.java
+++ 数据结构/数据结构/data_structure/data_structure/src/二叉树/AVLTree.java
@@ -2,7 +2,7 @@ package 二叉树;
 
 import java.util.Comparator;
 
-public class AVLTree<E> extends BST<E> {
+public class AVLTree<E> extends BBST<E> {
     public AVLTree() {
         this(null);
     }
@@ -68,6 +68,26 @@ public class AVLTree<E> extends BST<E> {
             }
         }
     }
+
+    @Override
+    protected void afterRotate(Node<E> grand, Node<E> parent, Node<E> child) {
+        super.afterRotate(grand, parent, child);
+
+        // 更新高度
+        updateHeight(grand);
+        updateHeight(parent);
+    }
+
+    @Override
+    protected void rotate(Node<E> r, Node<E> b, Node<E> c, Node<E> d, Node<E> e, Node<E> f) {
+        super.rotate(r, b, c, d, e, f);
+
+        // 更新高度
+        updateHeight(b);
+        updateHeight(f);
+        updateHeight(d);
+    }
+
     /**
      * 恢复平衡
      * @param grand 高度最低的那个不平衡节点
@@ -88,83 +108,6 @@ public class AVLTree<E> extends BST<E> {
                 rotate(grand, grand, parent.left, parent, node.left, node);
             }
         }
-    }
-
-    private void rotate(
-            Node<E> r, // 子树的根节点
-            Node<E> b, Node<E> c,
-            Node<E> d,
-            Node<E> e, Node<E> f) {
-        // 让d成为这棵子树的根节点
-        d.parent = r.parent;
-        if (r.isLeftChild()) {
-            r.parent.left = d;
-        } else if (r.isRightChild()) {
-            r.parent.right = d;
-        } else {
-            root = d;
-        }
-
-        //b-c
-        b.right = c;
-        if (c != null) {
-            c.parent = b;
-        }
-        updateHeight(b);
-
-        // e-f
-        f.left = e;
-        if (e != null) {
-            e.parent = f;
-        }
-        updateHeight(f);
-
-        // b-d-f
-        d.left = b;
-        d.right = f;
-        b.parent = d;
-        f.parent = d;
-        updateHeight(d);
-    }
-
-    private void rotateLeft(Node<E> grand) {
-        Node<E> parent = grand.right;
-        Node<E> child = parent.left;
-        grand.right = child;
-        parent.left = grand;
-        afterRotate(grand, parent, child);
-    }
-
-    private void rotateRight(Node<E> grand) {
-        Node<E> parent = grand.left;
-        Node<E> child = parent.right;
-        grand.left = child;
-        parent.right = grand;
-        afterRotate(grand, parent, child);
-    }
-
-    private void afterRotate(Node<E> grand, Node<E> parent, Node<E> child) {
-        // 让parent称为子树的根节点
-        parent.parent = grand.parent;
-        if (grand.isLeftChild()) {
-            grand.parent.left = parent;
-        } else if (grand.isRightChild()) {
-            grand.parent.right = parent;
-        } else { // grand是root节点
-            root = parent;
-        }
-
-        // 更新child的parent
-        if (child != null) {
-            child.parent = grand;
-        }
-
-        // 更新grand的parent
-        grand.parent = parent;
-
-        // 更新高度
-        updateHeight(grand);
-        updateHeight(parent);
     }
 
     private boolean isBalanced(Node<E> node) {
