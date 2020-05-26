aa2f85819963a5356778f49045fb1ebd1334cbff
邢雅杰 <470446087@qq.com>
2020-05-26 21:55:12 +0800

    原生代码

---
+++ Flutter/demo/native_demo/NativeDemo/NativeDemo/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  NativeDemo
+//
+//  Created by xingyajie on 2022/10/27.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
