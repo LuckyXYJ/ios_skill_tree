a51ef16776e28b7779d9028ea45748dd64754133
邢雅杰 <470446087@qq.com>
2020-06-27 21:15:12 +0800

    选择

--- Swift/Swift流程控制.md
+++ Swift/Swift流程控制.md
@@ -48,6 +48,8 @@ for - in 也可以使用带间隔的区间值 **stride(from: , through: , by: )*
 
 **逆序**：` ...`或者` ..<`都是从小到大，如果是需要逆序的情况下，可以使用`stride(from: 3, through: 0, by: -1)`实现，也可以使用` (0...10).reversed()`来实现
 
+**index**：遍历数组时可以通过` for i in 0..<arr.count`方式获取index。或者` for (index, item) in arr.enumerated()`
+
 ```
 let names = ["小王", "老王", "小任", "大任"];
 let range1: ClosedRange<Int> = 1...2	//闭区间
--- 算法/Algorithm/Algorithm/ArraySort.swift
+++ 算法/Algorithm/Algorithm/ArraySort.swift
@@ -7,12 +7,23 @@
 
 import Foundation
 
-class ArraySort {
+protocol ArraySort {
     
-    static func bublleSort(arr: [Int]) -> [Int] {
+    
+    /// 冒泡排序
+    static func sort(arr: [Int]) -> [Int]
+}
+
+
+/// 冒泡排序
+class BublleSort: ArraySort {
+    
+    /// 冒泡排序
+    static func sort(arr: [Int]) -> [Int] {
+        if arr.count <= 1 {return arr}
         var list = arr;
-        for end in (1..<list.count - 1).reversed() {
-            for j in 0...end {
+        for end in (0..<list.count - 1).reversed() {
+            for j in 0..<end {
                 if list[j] > list[j+1] {
                     (list[j], list[j+1]) = (list[j+1], list[j])
                 }
@@ -21,11 +32,13 @@ class ArraySort {
         return list
     }
     
-    static func bublleSort1(arr: [Int]) -> [Int] {
+    /// 冒泡排序优化，已完成排序的提前终止循环
+    static func sort1(arr: [Int]) -> [Int] {
+        if arr.count <= 1 {return arr}
         var list = arr;
         var sorted = true;
-        for end in (1..<list.count - 1).reversed() {
-            for j in 0...end {
+        for end in (0..<list.count - 1).reversed() {
+            for j in 0..<end {
                 if list[j] > list[j+1] {
                     (list[j], list[j+1]) = (list[j+1], list[j])
                     sorted = false
@@ -36,19 +49,19 @@ class ArraySort {
         return list
     }
     
-    static func bublleSort2(arr: [Int]) -> [Int] {
+    /// 冒泡排序优化，尾部有序部分位置记录，减少比较次数
+    static func sort2(arr: [Int]) -> [Int] {
+        if arr.count <= 1 {return arr}
         var list = arr;
-        
-        for var end in (1..<list.count - 1).reversed() {
+        for var end in (0..<list.count - 1).reversed() {
             
             var sortedIndex = 1;
-            for j in 0...end {
+            for j in 0..<end {
                 if list[j] > list[j+1] {
                     (list[j], list[j+1]) = (list[j+1], list[j])
                     sortedIndex = j + 1;
                 }
             }
-        
             // 没有效果，swift的for循环中，修改end不影响整体循环流程
             end = sortedIndex;
         }
@@ -57,3 +70,20 @@ class ArraySort {
 }
 
 
+/// 选择排序
+class SelectionSort: ArraySort {
+    static func sort(arr: [Int]) -> [Int] {
+        if arr.count <= 1 {return arr}
+        var list = arr;
+        for end in (0..<list.count-1).reversed(){
+            var maxIndex = 0;
+            for start in 0..<end {
+                if list[start] > list[maxIndex] {
+                    maxIndex = start;
+                }
+            }
+            (list[maxIndex], list[end]) = (list[end], list[maxIndex])
+        }
+        return list
+    }
+}
