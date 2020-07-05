2d62b2156607a74ca790e83860cd22543982b96c
邢雅杰 <470446087@qq.com>
2020-07-05 21:15:12 +0800

    二分查找

--- 算法/Algorithm/Algorithm/sort/InsertionSort.swift
+++ 算法/Algorithm/Algorithm/sort/InsertionSort.swift
@@ -43,3 +43,42 @@ class InsertionSort1: ArraySort {
         return list
     }
 }
+
+/// 插入排序优化2，在优化1的基础上，使用二分查找，减少对比次数
+class InsertionSort2: ArraySort {
+    
+    static func sort(arr: [Int]) -> [Int] {
+        if arr.count < 2 {return arr}
+        var list = arr
+        for i in 1..<list.count {
+            let dest = InsertionSort2.search(arr: list, index: i)
+            InsertionSort2.insert(arr: &list, source: i, dest: dest)
+        }
+        return list
+    }
+    
+    static func insert(arr: inout [Int], source: Int, dest: Int) {
+        
+        if dest == source { return }
+        let temp = arr[source];
+        for i in ((dest+1)...source).reversed() {
+            arr[i] = arr[i - 1];
+        }
+        arr[dest] = temp
+    }
+ 
+    static func search(arr: [Int], index: Int) -> Int {
+        var begin = 0
+        var end = index
+        
+        while(begin < end) {
+            let mid = (begin + end) >> 1
+            if (arr[mid] > arr[index]) {
+                end = mid
+            } else {
+                begin = mid + 1
+            }
+        }
+        return begin
+    }
+}
