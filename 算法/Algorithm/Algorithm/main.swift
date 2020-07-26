1b6984d2e7130904953475553da630a684ae3256
邢雅杰 <470446087@qq.com>
2020-07-26 21:15:12 +0800

    希尔排序

--- 算法/Algorithm/Algorithm/main.swift
+++ 算法/Algorithm/Algorithm/main.swift
@@ -10,7 +10,7 @@ import Foundation
 var array: [Int] = [2,5,3,1,4,8,10,7,6,9,11,12,21,35,12,6]
 
 //let arr = BublleSort.sort(arr: array);
-let arr = QuickSort.sort(arr: array);
+let arr = ShellSort.sort(arr: array);
 ArrayPrint.printArr(arr: arr);
 
 
